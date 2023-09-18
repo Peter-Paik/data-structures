10ce821a6bda0833fd4fd41103da5d871d582b25
Peter-Paik <pjpaik@stu.naperville203.org>
2023-09-18 14:14:32 -0500

    Failing to completed linked list thing

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
@@ -12,6 +13,13 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+        ListIterator<String> iterator = strings.listIterator();   
+        ListIterator<String> iterator2 = strings.listIterator(strings.size()-1);
+
+        while(iterator.hasNext()){
+            String str = iterator.next();
+            iterator.remove();
+
+        }
     }
 }
