ce46a9ceb62e2fd92d32da51a6a1ba7cb6b233c4
Peter-Paik <111479650+Peter-Paik@users.noreply.github.com>
2023-10-08 21:37:25 +0000

    HTML checker

--- Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
+++ Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
@@ -17,13 +17,28 @@ public class HTMLChecker
 {
     public static void main(String[] args)
     {
-        String filename = "src/TagSample1.html";
+        String filename = "Chapter 15 Activities/HTMLChecker/src/TagSample1.html";
 
         try (Scanner in = new Scanner(new File(filename)))
         {
             // Your code goes here
-            . . .
+            boolean proper=true;
+            Stack<String> tags = new Stack<String>();
 
+            while(in.hasNext() && proper == true){
+                String tag = in.next();
+                if(tag.indexOf("/") == -1){
+                    tags.push(tag);
+                }
+                else{ //contains "\", so is an closing tag
+                    if(!tags.pop().equals(tag.substring(0,tag.indexOf("/")) + tag.substring(tag.indexOf("/")+1)))
+                        proper = false;
+                }
+            }
+            if(proper)
+                System.out.println("The HTML sequence is properly nested");
+            else
+                System.out.println("The HTML sequence is not properly nested");
 
         } catch (FileNotFoundException e)
         {
